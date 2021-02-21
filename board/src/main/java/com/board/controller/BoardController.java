0261cb7c6fd95598fa5eded8a4d898c8e87c26cf
김선재 <masterpc1013@gmail.com>
2021-02-21 19:19:30 +0900

    댓글 조회

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.board.domain.BoardVO;
 import com.board.domain.Page;
+import com.board.domain.ReplyVO;
 import com.board.service.BoardService;
+import com.board.service.ReplyService;
 
 @Controller
 @RequestMapping("/board/*")
@@ -20,6 +22,9 @@ public class BoardController {
 
 	@Inject
 	BoardService service;
+	
+	@Inject
+	ReplyService replyService;
 
 	// 게시물 목록
 	@RequestMapping(value="/list", method=RequestMethod.GET)
@@ -137,6 +142,11 @@ public class BoardController {
 		BoardVO vo = service.view(bno);
 		
 		model.addAttribute("view", vo);
+		
+		// 댓글 조회
+		List<ReplyVO> reply = null;
+		reply = replyService.list(bno);
+		model.addAttribute("reply", reply);
 	}
 	
 	// 게시물 수정
